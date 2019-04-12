e2b90946101c4c07240c77a2718dddb612f75f5c
sunwize <molpick98@gmail.com>
2019-04-12 14:40:33 +0200

    ip update

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var dataTaskLayer = require('./dataLayer/dataTaskLayer');
 var uuidv4 = require("uuid/v4");
 
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
 require('./models/Task');
 
